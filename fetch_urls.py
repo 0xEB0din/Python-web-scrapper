4445f1720a427dc065722528330724970d1823d2
ahmedskaya <ahmd.ru@gmail.com>
2023-03-25 04:21:39 +0800

    fetch all urls within the body > section with the proper encoding

--- fetch_urls.py
+++ fetch_urls.py
@@ -1,35 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+from urllib.parse import urljoin, unquote
 
-def get_urls_from_page(url):
+def get_urls_from_page(url, error_file):
     try:
         response = requests.get(url)
         response.raise_for_status()
+        response.encoding = 'utf-8'
         soup = BeautifulSoup(response.text, 'html.parser')
         page_urls = set()
 
-        for link in soup.find_all('a', href=True):
-            href = link.get('href')
-            if href.lower().endswith('.pdf'):
-                full_url = urljoin(url, href)
-                page_urls.add(full_url)
+        body_element = soup.body
+        for section in body_element.find_all('section'):
+            for link in section.find_all('a', href=True):
+                href = link.get('href').strip()
+                decoded_href = unquote(href)
+                full_url = urljoin(url, decoded_href)
+                decoded_full_url = unquote(full_url)
+                page_urls.add(decoded_full_url)
 
         return page_urls
 
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
+        error_file.write(f"Error: {e}\n")
         return set()
 
 def main():
-    start_url = 'https://example.com'  # Replace with the URL you want to start with
-    all_urls = get_urls_from_page(start_url)
+    start_url = 'https://example.com'
+    
+    with open('result_links.txt', 'w', encoding='utf-8') as output_file, \
+         open('error_log.txt', 'w', encoding='utf-8') as error_file:
 
-    for url in all_urls:
-        print(f"URL: {url}")
-        sub_urls = get_urls_from_page(url)
-        for sub_url in sub_urls:
-            print(f"  - {sub_url}")
+        all_urls = get_urls_from_page(start_url, error_file)
+
+        for url in all_urls:
+            output_file.write(f"URL: {url}\n")
+            sub_urls = get_urls_from_page(url, error_file)
+            for sub_url in sub_urls:
+                output_file.write(f"  - {sub_url}\n")
 
 if __name__ == '__main__':
     main()
