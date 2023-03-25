85008010ca14c4c9e4d51732f571dfc824475913
ahmedskaya <ahmd.ru@gmail.com>
2023-03-26 07:54:03 +0800

    progress bar and stats added

--- fetch_urls.py
+++ fetch_urls.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, unquote
+from tqdm import tqdm
+from termcolor import colored
 
 def get_urls_from_page(url, error_file):
     try:
@@ -28,18 +30,32 @@ def get_urls_from_page(url, error_file):
 
 def main():
     start_url = 'https://www.riwaya.ga/riwayat_3alamiya.htm'
-    
+
     with open('result_links.txt', 'w', encoding='utf-8') as output_file, \
-         open('error_log.txt', 'w', encoding='utf-8') as error_file:
+            open('error_log.txt', 'w', encoding='utf-8') as error_file, \
+            open('subpage_links.txt', 'w', encoding='utf-8') as subpage_file:
 
         all_urls = get_urls_from_page(start_url, error_file)
 
-        for url in all_urls:
+        total_urls = 0
+        total_sub_urls = 0
+        total_errors = 0
+
+        for url in tqdm(all_urls, desc="Processing URLs", unit="url"):
             output_file.write(f"URL: {url}\n")
             sub_urls = get_urls_from_page(url, error_file)
+            total_sub_urls += len(sub_urls)
             for sub_url in sub_urls:
-                if sub_url.lower().endswith('.pdf'):
-                    output_file.write(f"PDF URL: {sub_url}\n")
+                subpage_file.write(f"Subpage URL: {sub_url}\n")
+            total_urls += 1
+
+        with open('error_log.txt', 'r', encoding='utf-8') as error_file:
+            total_errors = sum(1 for line in error_file if "Error:" in line)
+
+        print(colored("\nProcessing completed!", "green"))
+        print(colored(f"Total URLs processed: {total_urls}", "cyan"))
+        print(colored(f"Total sub-URLs found: {total_sub_urls}", "cyan"))
+        print(colored(f"Total errors encountered: {total_errors}", "red"))
 
 if __name__ == '__main__':
     main()
